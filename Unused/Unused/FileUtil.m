445a74dc194a5afb3e1b9ffd7c3d34658dfee9f5
James Stout <stoutyhk@gmail.com>
2020-11-25 13:50:46 +0800

    added pdf

--- Unused/Unused/FileUtil.m
+++ Unused/Unused/FileUtil.m
@@ -48,25 +48,14 @@
 }
 
 + (NSArray *)imageFilesInDirectory:(NSString *)directoryPath {
-    
-    NSMutableArray *images = [NSMutableArray array];
-    
-    // jpg
-    NSArray *jpg = [self searchDirectory:directoryPath forFiletype:@"jpg"];
-    [images addObjectsFromArray:jpg];
 
-    // jpeg
-    NSArray *jpeg = [self searchDirectory:directoryPath forFiletype:@"jpeg"];
-    [images addObjectsFromArray:jpeg];
-    
-    // png
-    NSArray *png = [self searchDirectory:directoryPath forFiletype:@"png"];
-    [images addObjectsFromArray:png];
-    
-    // gif
-    NSArray *gif = [self searchDirectory:directoryPath forFiletype:@"gif"];
-    [images addObjectsFromArray:gif];
-    
+    NSMutableArray *images = [NSMutableArray array];
+
+    for (NSString *filetype in @[@"jpg", @"jpeg", @"png", @"gif", @"pdf"]) {
+        NSArray *array = [self searchDirectory:directoryPath forFiletype:filetype];
+        [images addObjectsFromArray:array];
+    }
+
     return images;
 }
 
